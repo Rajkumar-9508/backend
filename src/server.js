ba12fae525759ae33c7a27c82066a1e8908e0bc4
rajkumar <berarajkumar713@gmail.com>
2025-09-06 20:58:16 +0530

    Add professional README for SkillSync

--- src/server.js
+++ src/server.js
@@ -52,10 +52,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
 
 
 // Sessions (JWT ke bina)
